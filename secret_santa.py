bca18ea493aa0a7ce543577ba0b58edee33f336f
Eric Cozzi <eric@cozzi.us>
2025-11-09 11:47:38 -0600

    more entropy

--- secret_santa.py
+++ secret_santa.py
@@ -9,32 +9,64 @@ from envs import APP_PASSWORD, USER_MAP
 from email.mime.text import MIMEText
 
 PREVIOUS_DERANGEMENT_FILE = "previous_derangement.json"
+DEFAULT_CHECK_HISTORICAL_DEPTH = 2
 
 
-def derange_list(user_list, previous_list=None):
+def check_historical_derangements(user_list, historical_derangements):
+    """
+    Checks if the current derangement has occurred in previous derangements.
+    """
+    # print(f"{user_list=}")
+
+    from pprint import pprint
+
+    pprint(historical_derangements)
+    if historical_derangements is not None:
+        check_historical_depth = min(
+            DEFAULT_CHECK_HISTORICAL_DEPTH, len(historical_derangements)
+        )
+        for d in range(check_historical_depth):
+            derangement = list(historical_derangements[-(d + 1)].values())
+            # print(f"Checking against historical derangement {d} of {len(historical_derangements)}: {derangement}")
+            if any(
+                user_list[i - 1] == derangement[i - 1] for i in range(len(user_list))
+            ):
+                # print("Match found with historical derangement.")
+                return False
+        # print("No matches found with historical derangements.")
+        return True
+    # print("No historical derangements to check against.")
+    return True
+
+
+def derange_list(user_list, historical_assignments=None):
     """
     Randomizes a list such that no element remains in its original position.
     This function shuffles the list repeatedly until a derangement is found.
     """
+    random.seed()
+    # print(f"{user_list=}")
     if not user_list:
         return []
 
     # Create a copy to avoid modifying the original during checks
-    shuffled_list = list(user_list)
+    shuffled_list = user_list.copy()
+    # random.shuffle(shuffled_list)
+    # print(f"{user_list=}")
+    # print(f"{shuffled_list=}")
 
     while True:
-        while any(shuffled_list[i] == user_list[i] for i in range(len(user_list))):
+        for _ in range(random.randint(1, 100)):
             random.shuffle(shuffled_list)
 
-        if previous_list:
-            # Ensure that everyone has a different recipient than last time
-            if any(
-                shuffled_list[i] == previous_list[i] for i in range(len(previous_list))
-            ):
+        while any(shuffled_list[i] == user_list[i] for i in range(len(user_list))):
+            for _ in range(random.randint(1, 100)):
                 random.shuffle(shuffled_list)
-                continue
 
-        break
+        # print(f"trying {shuffled_list=}")
+
+        if check_historical_derangements(shuffled_list, historical_assignments):
+            break
 
     return shuffled_list
 
@@ -67,15 +99,14 @@ def load_list_of_santa_maps():
     return None
 
 
-def load_previous_santa_map():
-    list_of_santa_maps = load_list_of_santa_maps()
+def load_previous_santa_map(list_of_santa_maps=None):
     if list_of_santa_maps:
         previous_santa_map = list_of_santa_maps[-1]
         return previous_santa_map
     return {}
 
 
-def get_recipients_from_santa_map(previous_santa_map:dict[str, str]):
+def get_recipients_from_santa_map(previous_santa_map: dict[str, str]):
     if previous_santa_map:
         previous_recipients = list(previous_santa_map.values())
         return previous_recipients
@@ -96,10 +127,11 @@ def main():
     secret_santas = list(USER_MAP.keys())
     recipient_emails = list(USER_MAP.values())
 
-    previous_santa_map = load_previous_santa_map()
+    list_of_santa_maps = load_list_of_santa_maps()
+    previous_santa_map = load_previous_santa_map(list_of_santa_maps)
     previous_recipients_list = get_recipients_from_santa_map(previous_santa_map)
 
-    new_recipients_list = derange_list(secret_santas, previous_recipients_list)
+    new_recipients_list = derange_list(secret_santas, list_of_santa_maps)
 
     if True:
         for secret_santa, recipient_email, family_member in zip(
@@ -110,7 +142,7 @@ def main():
                 body=f"{secret_santa}, you are secret santa for {family_member} this coming week. Use the document here to record and see Secret Santa ideas: https://docs.google.com/document/d/1VWQvmUEsggtSWMkpB7U1d8athg7qmb3w86-yT94Ja0c/edit?usp=sharing",
                 sender="eric@cozzi.us",
                 recipients=[
-                   recipient_email,
+                    recipient_email,
                 ],
             )
             print(f"Emailed {secret_santa}.")
@@ -119,5 +151,28 @@ def main():
     save_new_santa_map(new_santa_map)
 
 
+def test():
+    from pprint import pprint
+    from prettytable import PrettyTable
+
+    historical = []
+    users = USER_MAP.copy()
+    for _ in range(20):
+        secret_santas = list(users.keys())
+        # pprint(historical, indent=4)
+        new_recipients_list = derange_list(secret_santas, historical)
+        new_santa_map = dict(zip(secret_santas, new_recipients_list))
+        # pprint(new_santa_map, sort_dicts=False, indent=4)
+        historical.append(new_santa_map)
+
+    table = PrettyTable(users.keys())
+    for derangement in historical:
+        row = [derangement[user] for user in users.keys()]
+        table.add_row(row)
+
+    print(table)
+
+
 if __name__ == "__main__":
+    # test()
     main()
